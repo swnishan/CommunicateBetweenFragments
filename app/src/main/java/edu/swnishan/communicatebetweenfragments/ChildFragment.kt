f07c96ce4417f16e13b30d8c3986d379e15bb35f
swnishan <swnishan@gmail.com>
2020-11-07 15:18:16 +0100

    add lister in child fragment

--- app/src/main/java/edu/swnishan/communicatebetweenfragments/ChildFragment.kt
+++ app/src/main/java/edu/swnishan/communicatebetweenfragments/ChildFragment.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.os.bundleOf
 import androidx.fragment.app.setFragmentResult
+import androidx.fragment.app.setFragmentResultListener
 import kotlinx.android.synthetic.main.fragment_child.*
 
 
@@ -22,9 +24,17 @@ class ChildFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        //pass result to ParentFragment through FragmentManager (ParentFragmentManager)
         button_parent.setOnClickListener {
-            // set fragment result to the parent fragment
-            setFragmentResult("key_parent", bundleOf("say_hi" to "Hi parent fragment"))
+            setFragmentResult("key_parent", bundleOf("say_hi_parent" to "Hi parent fragment"))
+        }
+
+        //listen result from the ParentFragment through FragmentManager (ParentFragmentManager)
+        //Once result receive show the Toast
+        setFragmentResultListener("key_child") {key, result->
+            // get the result from bundle
+            val stringResult = result.getString("say_hi_child")
+            Toast.makeText(requireContext(), "$key: $stringResult", Toast.LENGTH_SHORT).show()
         }
     }
 }
