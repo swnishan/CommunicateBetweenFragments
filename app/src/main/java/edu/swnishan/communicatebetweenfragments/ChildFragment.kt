eef2e493e990018c9e5b070db26e40f9a39a53fd
swnishan <swnishan@gmail.com>
2020-11-07 15:06:41 +0100

    pass data from child

--- app/src/main/java/edu/swnishan/communicatebetweenfragments/ChildFragment.kt
+++ app/src/main/java/edu/swnishan/communicatebetweenfragments/ChildFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.fragment.app.setFragmentResult
+import kotlinx.android.synthetic.main.fragment_child.*
 
 
 class ChildFragment : Fragment() {
@@ -19,5 +22,9 @@ class ChildFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        button_parent.setOnClickListener {
+            // set fragment result to the parent fragment
+            setFragmentResult("key_parent", bundleOf("say_hi" to "Hi parent fragment"))
+        }
     }
 }
