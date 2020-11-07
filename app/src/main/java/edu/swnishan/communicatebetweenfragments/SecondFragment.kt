c65aa318a08741b4ec4be3b9412cfb68a210e120
swnishan <swnishan@gmail.com>
2020-11-07 15:24:41 +0100

    add listener to second fragment

--- app/src/main/java/edu/swnishan/communicatebetweenfragments/SecondFragment.kt
+++ app/src/main/java/edu/swnishan/communicatebetweenfragments/SecondFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.fragment.app.setFragmentResult
 import kotlinx.android.synthetic.main.fragment_second.*
 
 
@@ -20,7 +22,10 @@ class SecondFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        button_previous.setOnClickListener {parentFragmentManager.popBackStack()}
-
+        button_previous.setOnClickListener {
+            //pass result to the FirstFragment through FragmentManager
+            setFragmentResult("key_previous", bundleOf("say_hi_previous" to "Hi first fragment"))
+            parentFragmentManager.popBackStack()
+        }
     }
 }
