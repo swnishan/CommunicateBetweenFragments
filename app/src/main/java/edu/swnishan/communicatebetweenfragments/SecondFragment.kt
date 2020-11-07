8ede6d39652fcafa80d62b54fa4bcd53bf2d7e13
swnishan <swnishan@gmail.com>
2020-11-07 14:39:33 +0100

    add back from second fragment

--- app/src/main/java/edu/swnishan/communicatebetweenfragments/SecondFragment.kt
+++ app/src/main/java/edu/swnishan/communicatebetweenfragments/SecondFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_second.*
 
 
 class SecondFragment : Fragment() {
@@ -18,6 +19,8 @@ class SecondFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        button_previous.setOnClickListener {parentFragmentManager.popBackStack()}
 
     }
 }
